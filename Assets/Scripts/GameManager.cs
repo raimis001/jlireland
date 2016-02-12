c641e0179b62a49cc230f9baa7e4a8e98593acb5
Raimonds <raimis0001@gmail.com>
2016-02-12 15:14:56 +0200

    init

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+	private static GameManager _instance = null;
+	public static GameManager Instance { get { return _instance; } }
+
+	public delegate void HourChange(int hours);
+	public static event HourChange OnHourChange;
+
+	private float HourTime = 2;
+	private float _hourTime;
+
+	void Awake()
+	{
+		_instance = this;
+	}
+
+	// Use this for initialization
+	void Start()
+	{
+		_hourTime = HourTime;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		_hourTime -= Time.smoothDeltaTime;
+		if (_hourTime <= 0)
+		{
+			if (OnHourChange != null)
+			{
+				OnHourChange(1);
+			}
+			_hourTime = HourTime;
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : MonoBehaviour
+{
+	private static PlayerManager _instance = null;
+	public static PlayerManager Instance { get { return _instance; } }
+
+	private float _tired = 0;
+
+	public float Tired
+	{
+		get
+		{
+			return _tired;
+		}
+		set
+		{
+			_tired = value;
+			if (_tired > 100)
+			{
+				_tired = 100;
+			}
+		}
+
+	}
+
+	public float TiredMidifier
+	{
+		get
+		{
+			return 0;
+		}
+	}
+
+	void Awake()
+	{
+		_instance = this;
+	}
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	void OnEnable()
+	{
+		GameManager.OnHourChange += OnHourChange;
+	}
+
+	void OnDisable()
+	{
+		GameManager.OnHourChange -= OnHourChange;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	void OnHourChange(int hours)
+	{
+		Tired += TiredMidifier;
+	}
+}
